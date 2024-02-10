9cfee5b4b69d7718a997f87e6f69d6a2480d8453
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-11 01:30:36 +0600

    snippet serializers

---
+++ snippets/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, max_length=100, allow_blank=True)
+    code = serializers.CharField(style={"base_template": "textarea.html"})
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
+    linenos = serializers.BooleanField(required=False)
+
+    def create(self, validated_data):
+        Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.code = validated_data.get("code", instance.code)
+        instance.language = validated_data.get("language", instance.language)
+        instance.style = validated_data.get("style", instance.style)
+        instance.linenos = validated_data.get("linenos", instance.linenos)
+        instance.save()
+        return instance
