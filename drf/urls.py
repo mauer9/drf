4206ffeb66a360c6bc30af1853e7f1233f9ee721
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-11 01:22:40 +0600

    0

---
+++ drf/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+# router.register("users", views.UserViewSet)
+# router.register("groups", views.GroupViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
+
+urlpatterns += router.urls
