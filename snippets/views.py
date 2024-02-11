8b7cb587a077c8c39397f66116f5fc42ccedeb24
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-11 22:07:32 +0600

    POST snippet

--- snippets/views.py
+++ snippets/views.py
@@ -7,7 +7,16 @@ from rest_framework.parsers import JSONParser
 
 @csrf_exempt
 def snippet_list(request):
+    """get all snippets list or create a snippet"""
     if request.method == "GET":
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
