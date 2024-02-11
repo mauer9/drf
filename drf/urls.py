d418242044bf7d0352ad342df5ebb788adb1943f
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-11 22:56:57 +0600

    snippets urls

--- drf/urls.py
+++ drf/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
-from rest_framework import routers
-from snippets import views
 
 
 urlpatterns = [
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path("snippets/", include("snippets.urls")),
 ]
