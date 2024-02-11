c066efaa11b9f8231e45f1e1f50aee2fc84ef1f3
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-11 23:30:15 +0600

    include("snippets.url") does not format suffix

--- drf/urls.py
+++ drf/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from snippets import views
 
 
 urlpatterns = [
-    path("snippets/", include("snippets.urls")),
+    path("snippets", views.snippet_list),
+    path("snippets/<int:pk>/", views.snippet_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
