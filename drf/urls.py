8862bae3b5aae654d48e1d40c5b928636ae150cf
mauer <75573495+mauer9@users.noreply.github.com>
2024-02-20 22:27:56 +0600

    router

--- drf/urls.py
+++ drf/urls.py
@@ -1,37 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
-from snippets.views import api_root, SnippetViewSet, UserViewSet
+from snippets.views import SnippetViewSet, UserViewSet
 
 
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
-
-snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
-snippet_detail = SnippetViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }
-)
-snippet_highlight = SnippetViewSet.as_view(
-    {
-        "get": "highlight",
-    }
-)
+router = DefaultRouter()
+router.register("snippets", SnippetViewSet, basename="snippet")
+router.register("users", UserViewSet, basename="user")
 
 urlpatterns = [
-    path("", api_root),
-    path("users/", user_list, name="user-list"),
-    path("users/<int:pk>/", user_detail, name="user-detail"),
-    path("snippets/", snippet_list, name="snippet-list"),
-    path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
-    path(
-        "snippets/<int:pk>/highlight",
-        snippet_highlight,
-        name="snippet-highlight",
-    ),
+    path("", include(router.urls)),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -2,23 +2,12 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, action
-from rest_framework.relations import reverse
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(["GET"])
-def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "snippets": reverse("snippet-list", request=request, format=format),
-        }
-    )
 
 
 class SnippetViewSet(ModelViewSet):
